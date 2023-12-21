5f6e8904b2335e3a485a32722dca4ae3a4a1b0a5
Abhishek150811 <abhishekaps2003@gmail.com>
2023-12-21 22:27:39 +0530

    updated clock with different time zone

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
-function func(){
-    d = new Date() ;
-    hours = d.getHours() ;
-    minutes = d.getMinutes() ;
-    seconds = d.getSeconds() ;
-    document.getElementsByClassName('hour')[0].innerHTML = hours ; 
-    document.getElementsByClassName('min')[0].innerHTML = minutes ; 
-    document.getElementsByClassName('sec')[0].innerHTML = seconds ; 
+function timechange() {
+    d = new Date();
+    hours = d.getHours();
+    minutes = d.getMinutes();
+    seconds = d.getSeconds();
+    // if(active == 'london'){
+    //     if(minutes - 30 < 0){
+    //         minutes = minutes + 30 ; 
+    //         hours = hours + 1 ; 
+    //     }
+    //     else{
+    //         minutes = minutes - 30 ;
+    //     }
+    //     hours = hours - 5 ; 
+    // }
+    // else if(active == 'washington' || active == 'ottawa'){
+    //     if(minutes - 30 < 0){
+    //         minutes = minutes + 30 ; 
+    //         hours = hours + 1 ; 
+    //     }
+    //     else{
+    //         minutes = minutes - 30 ; 
+    //     }
+    //     hours = hours - 10 ; 
+    // }
+    // console.log(hours, minutes, seconds);
+    document.getElementsByClassName('hour')[0].innerHTML = hours;
+    document.getElementsByClassName('min')[0].innerHTML = minutes;
+    document.getElementsByClassName('sec')[0].innerHTML = seconds;
 }
 
-function main(){
-    setInterval(func , 1000) ;
+function main() {
+    setInterval(timechange, 1000);
+
+    function func(event) {
+        active = this.id;
+        console.log(active );
+        delhi.className = '';
+        ottawa.className = '';
+        washington.className = '';
+        london.className = '';
+        this.className = 'active'
+    }
+    const delhi = document.getElementById('new-delhi');
+    const ottawa = document.getElementById('ottawa');
+    const washington = document.getElementById('washington');
+    const london = document.getElementById('london');
+    var active = 'new-delhi';
+
+    delhi.addEventListener('click' , func) ;
+    ottawa.addEventListener('click' , func) ;
+    washington.addEventListener('click' , func) ;
+    london.addEventListener('click' , func) ;
+    // delhi.onclick = func;
+    // ottawa.onclick = func;
+    // washington.onclick = func;
+    // london.onclick = func;
+
 }
-main() ;
+main();
